94b941c94d4c296b7c9ffc4e729c3fa3e7d9557b
ggang89 <ggang89@gmail.com>
2024-07-13 13:39:09 +0900

    input창 이벤트 연결=> onchange,onClick(추가 버튼 누르면 새로운 객체를 만들어서 list에 넣어줘야함=>객체(id,title,isEditing)에 필요한 uuid설치(npm i uuid)

--- src/components/todoContent.js
+++ src/components/todoContent.js
@@ -1,23 +1,38 @@
-import { useState } from "react"
-
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 export default function TodoContents() {
-  const [addTodo, setAddTodo] = useState(" ")
-  const addBtn = () => {
-    
-  }
+  const [addTodo, setAddTodo] = useState(" ");
+  const [todoList, setTodoList] = useState([]);
+
+  const addTodoBtn = () => {
+    const newArr = {
+      id: uuidv4(),
+      todoTitle: addTodo,
+      isEdting: false,
+    };
+
+    setTodoList([newArr, ...todoList]);
+    setAddTodo(" ");
+  };
+  const handleAddtext = (e) => {
+    setAddTodo(e.target.value);
+  };
   return (
     <>
       <div>
         {/* 새로운 투두 추가창 */}
         <label htmlFor="addTodo">new Todo</label>
-        <input id="addTodo" value={addTodo} type="text"></input>
-        <button onClick={addBtn}>추가</button>
+        <input
+          id="addTodo"
+          value={addTodo}
+          type="text"
+          onChange={handleAddtext}
+        ></input>
+        <button onClick={addTodoBtn}>추가</button>
       </div>
-      
-      {/* 투두 리스트 */}
-      <ul>
 
-      </ul>
+      {/* 투두 리스트 */}
+      <ul></ul>
     </>
-  )
+  );
 }
